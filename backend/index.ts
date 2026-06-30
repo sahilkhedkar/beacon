bf9c24bc7b83038c259765999267e0d0c0531a64
Sahil Khedkar <sahilkhedkar11@gmail.com>
2026-06-30 18:08:54 +0530

    feat: changed the format for streamback response

--- backend/index.ts
+++ backend/index.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import { tavily } from "@tavily/core";
-import { Output, streamText } from "ai";
+import { streamText } from "ai";
 import { PROMPT_TEMPLATE, SYSTEM_PROMPT } from "./prompts";
 import { z } from "zod";
+import { openai } from "@ai-sdk/openai";
 
 dotenv.config();
 
@@ -36,20 +37,23 @@ app.post("/chat", async (req, res) => {
     .replace("{{USER_QUERY}}", query);
 
   const result = streamText({
-    model: 'openai/gpt-5.4',
+    model: openai("gpt-4.1"),
     prompt: prompt,
     system: SYSTEM_PROMPT,
   });
+
+  res.header("Cache-Control" , "no-cache");
+  res.header("Content-Type" , "text/event-stream")
 
 
   for await ( const textPart of result.textStream) {
     res.write(textPart);
   }
 
-  res.write("----------SOURCES-----------\n")
+  res.write("\n----------SOURCES-----------\n")
 
   // also stream back the sources and the follow up questions
-    webSearchResults.forEach(result => res.write(JSON.stringify(result)))
+    res.write(JSON.stringify(webSearchResults.map( result => ({ url : result.url }))));
 
   // close the event stream
 
