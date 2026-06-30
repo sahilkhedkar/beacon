3d42290c9f5f9b78b1664b689213ce50202c1e89
Sahil Khedkar <sahilkhedkar11@gmail.com>
2026-06-30 16:09:38 +0530

    rm: removed the output schema

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import { tavily } from "@tavily/core";
-import { streamText } from "ai";
+import { Output, streamText } from "ai";
+import { PROMPT_TEMPLATE, SYSTEM_PROMPT } from "./prompts";
+import { z } from "zod";
 
 dotenv.config();
 
@@ -29,10 +31,20 @@ app.post("/chat", async (req, res) => {
   // hit the llm and stream back the response
   // llm vercel ai gateway
 
+  const prompt = PROMPT_TEMPLATE
+    .replace("{{WEB_SEARCH_RESULTS}}", JSON.stringify(webSearchResults))
+    .replace("{{USER_QUERY}}", query);
+
   const result = streamText({
     model: 'openai/gpt-5.4',
-    prompt: 'Invent a new holiday and describe its traditions.',
+    prompt: prompt,
+    system: SYSTEM_PROMPT,
   });
+
+
+  for await ( const textPart of result.textStream) {
+    res.end(textPart);
+  }
 
   // also stream back the sources and the follow up questions
 
