d4603884af840bb51af6f704cd874577387d528f
Sahil <sahilkhedkar11@gmail.com>
2026-09-02 23:47:30 +0530

    feat: add tavily

--- apps/api/index.ts
+++ apps/api/index.ts
@@ -2,18 +2,24 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.post("/chat" , (req , res) => {
-    // get query from the user
+    // step 1 : get query from the user
+    const { query } = req.body;
+    // step 2 : make sure user has credits/access to hit the endpoint
 
-    // make sure user has credits/access to hit the endpoint
+    // step 3 : check if we have web search indexed for a similar query
 
-    // check if we have web search indexed for a similar query
+    // step 4 : web search to gather resources
 
-    // web search to gather resources
+    // step 5 : do some context engineering on the prompt + web search responses
 
-    // do some context engineering on the prompt + web search responses
+    // step 6 : hit the LLM and stream the response back to the user
 
-    // hit the LLM and stream the response back to the user
+    // step 7 : also stream back the sources and the follow up questions ( which we can get from another parallel LLM call )
+
+    // step 8 : close the event stream
 })
 
 app.listen(3000);
